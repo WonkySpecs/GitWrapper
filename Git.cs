315cf94b3e9e227b0b51ccb5dbec27624831987a
Will Taylor <w1ll100@hotmail.co.uk>
2023-04-14 18:27:52 +0100

    Use file-scoped namespace

--- Git.cs
+++ Git.cs
@@ -1,49 +1,48 @@
 ﻿using System.Diagnostics;
 
-namespace GitWrapper
+namespace GitWrapper;
+
+public class Git
 {
-    public class Git
+    private readonly string _repoDir;
+
+    public Git(string repoDir)
     {
-        private readonly string _repoDir;
+        _repoDir = repoDir;
+    }
 
-        public Git(string repoDir)
-        {
-            _repoDir = repoDir;
-        }
+    public string Status() => Run("status");
 
-        public string Status() => Run("status");
+    public string HeadRef(bool shortHash=true) =>
+        Run("rev-parse", shortHash ? "--short" : "", "HEAD");
 
-        public string HeadRef(bool shortHash=true) =>
-            Run("rev-parse", shortHash ? "--short" : "", "HEAD");
+    public string Branch() => Run("rev-parse", "--abbrev-ref", "HEAD");
 
-        public string Branch() => Run("rev-parse", "--abbrev-ref", "HEAD");
+    public string Exec(string command) => Run(command);
 
-        public string Exec(string command) => Run(command);
-
-        private string Run(params string[] args)
-        {
-            var command = string.Join(
-                    " ",
-                    new List<string>
-                    {
-                        $"--git-dir=\"{Path.Join(_repoDir, ".git")}\"",
-                        $"--work-tree=\"{_repoDir}\"",
-                    }.Concat(args));
-
-            var p = new Process()
+    private string Run(params string[] args)
+    {
+        var command = string.Join(
+            " ",
+            new List<string>
             {
-                StartInfo = new ProcessStartInfo("git")
-                {
-                    Arguments = command,
-                    RedirectStandardInput = true,
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                }
-            };
-            p.Start();
-            var res = p.StandardOutput.ReadToEnd();
-            p.Close();
-            return res.Trim();
-        }
+                $"--git-dir=\"{Path.Join(_repoDir, ".git")}\"",
+                $"--work-tree=\"{_repoDir}\"",
+            }.Concat(args));
+
+        var p = new Process
+        {
+            StartInfo = new ProcessStartInfo("git")
+            {
+                Arguments = command,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+            }
+        };
+        p.Start();
+        var res = p.StandardOutput.ReadToEnd();
+        p.Close();
+        return res.Trim();
     }
 }
